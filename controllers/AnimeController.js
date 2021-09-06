bbaa433491fa165775184fe1d4f9333bd9f996d2
Micah <micahstewart324@gmail.com>
2021-09-05 20:03:50 -0400

    delete commented out6 and spacing 7

--- controllers/AnimeController.js
+++ controllers/AnimeController.js
@@ -3,11 +3,8 @@ const { Anime } = require('../models')
 const createAnime = async (req,res)=> {
   console.log(req.body);
   try{
-    //making new object
   const newAnime = await new Anime(req.body)
-  //store it inside database
   await newAnime.save()
-  //resolve
   return res.status(201).json(newAnime)
   } catch(error){
   console.log(error)
@@ -24,7 +21,6 @@ const allAnime = async (req,res)=>{
     console.log(error);
   }
 }
-
 
 const deleteAnime = async (req, res) => {
     try {
