2fd77d3c6adb8c09ae6499bdc2a85c6222155553
Micah <micahstewart324@gmail.com>
2023-02-14 16:06:20 -0500

    im back

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
 <div>
   <header>
   <Switch>
-  <Route exact path="/" component={User}/>
+  <Route exact className="Userpage" path="/" component={User}/>
   <Route exact path="/anime" component={Anime}/>
   <Route exact path="/watchlist" component={WatchList}/>
   </Switch>
